58bee0a8e5288ec14fe3cfb4c63341018c927741
Richard <richard-mank@hotmail.nl>
2024-10-23 15:07:14 -0400

    Het is nu weer redelijk compleet.

---
+++ controllers/ShareController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace controllers;
+
+use http\Request;
+use services\DatabaseContext;
+
+class ShareController {
+
+    public function __construct(
+        private readonly Request $request,
+        private DatabaseContext $ctx)
+    { }
+
+
+    public function handle() {
+        
+
+        if ($this->request->getRequestMethod() == 'POST') {
+
+            $share = $this->databaseContext->createShare(...$this->request->getPostValuesAll(['title', 'body', 'link','user_id']));                //...$this->request->getPostValuesAll(['title', 'body', 'link'])
+            //     $this->request->getPostValue('title'),
+            //     $this->request->getPostValue('body'),
+            //     $this->request->getPostValue('link')
+            // );
+
+            if ($share) {
+                return '<h1>aangemaakt hoor</h1>';
+            } else {
+                return '<h2>Alles is stuk</h2>';
+            }
+
+            // if ($share) {
+            //     return '<h1>Bijgewerkt hoor</h1>';
+            // } else {
+            //     return '<h2>Alles is stuk</h2>';
+            // }
+
+        } else {
+            $shares = file_get_contents('views/head.partial.html') . file_get_contents('views/navbar.partial.html') . file_get_contents('views/share.partial.html'); // + file_get_contents('views/shares.partial.html')
+            return $shares;
+        }
+    }
+}
